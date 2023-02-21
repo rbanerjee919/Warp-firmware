8bd70c4df962fab5a62fad6362a8741c2d007fd0
Rahul Banerjee <rahulbanerjee.friend@gmail.com>
2023-02-21 21:49:11 +0000

    INA219 major changes for coursework 3

---
+++ src/boot/ksdk1.1.0/devINA219.c
@@ -0,0 +1,267 @@
+#include <stdint.h>
+#include <stdlib.h>
+ #include <math.h>
+
+#include "config.h"
+
+#include "fsl_misc_utilities.h"
+#include "fsl_device_registers.h"
+#include "fsl_i2c_master_driver.h"
+#include "fsl_spi_master_driver.h"
+#include "fsl_rtc_driver.h"
+#include "fsl_clock_manager.h"
+#include "fsl_power_manager.h"
+#include "fsl_mcglite_hal.h"
+#include "fsl_port_hal.h"
+
+#include "gpio_pins.h"
+#include "SEGGER_RTT.h"
+#include "warp.h"
+
+#include "devINA219.h"
+
+#define INA219_DEBUG 0
+
+extern volatile WarpI2CDeviceState    deviceINA219State;
+extern volatile uint32_t        gWarpI2cBaudRateKbps;
+extern volatile uint32_t        gWarpI2cTimeoutMilliseconds;
+extern volatile uint32_t        gWarpSupplySettlingDelayMilliseconds;
+
+/*
+ *	Override Warp firmware's use of these pins and define new aliases.
+ */
+enum
+{
+	kINA219PinSCL		= GPIO_MAKE_PIN(HW_GPIOA, 8),
+	kINA219PinSDA		= GPIO_MAKE_PIN(HW_GPIOA, 9),
+};
+
+void
+initINA219(const uint8_t i2cAddress, uint16_t operatingVoltageMillivolts)
+{
+	/*
+	 *	Override Warp firmware's use of these pins.
+	 *
+	 *	Re-configure I2C to be on PTA8 and PTA9 for SCL and SDA respectively.
+	 */
+    
+    deviceINA219State.i2cAddress            = i2cAddress;
+    deviceINA219State.operatingVoltageMillivolts    = operatingVoltageMillivolts;
+    
+    
+    devINA219writeRegister(kINA219RegisterConfig, config.raw_val);
+    devINA219writeRegister(kINA219RegisterCalibration, INA219_CAL);
+
+    OSA_TimeDelay(100);
+
+    devINA219writeRegisterPointer(kINA219RegisterCurrent);
+
+    
+    PORT_HAL_SetMuxMode(PORTA_BASE, 8u, kPortMuxAlt2);
+    PORT_HAL_SetMuxMode(PORTA_BASE, 9u, kPortMuxAlt2);
+    
+    return;
+}
+
+WarpStatus
+devINA219writeRegister(INA219Register deviceRegister, uint16_t payload)
+{
+    uint8_t        payloadByte[2], commandByte[1];
+    i2c_status_t    status;
+
+    if (deviceRegister > 0x05) {
+        return kWarpStatusBadDeviceCommand;
+    }
+
+    i2c_device_t slave =
+    {
+        .address = deviceINA219State.i2cAddress,
+        .baudRate_kbps = gWarpI2cBaudRateKbps
+    };
+
+    warpScaleSupplyVoltage(deviceINA219State.operatingVoltageMillivolts);
+    commandByte[0] = deviceRegister;
+    payloadByte[0] = (uint8_t) (payload >> 8);
+    payloadByte[1] = (uint8_t) (payload & 0xFF);
+    warpEnableI2Cpins();
+
+    status = I2C_DRV_MasterSendDataBlocking(
+                            0 /* I2C instance */,
+                            &slave,
+                            commandByte,
+                            1,
+                            payloadByte,
+                            2,
+                            gWarpI2cTimeoutMilliseconds);
+
+    if (status != kStatus_I2C_Success)
+    {
+        return kWarpStatusDeviceCommunicationFailed;
+    }
+
+    return kWarpStatusOK;
+}
+
+WarpStatus
+devINA219writeRegisterPointer(INA219Register deviceRegister)
+{
+    uint8_t        commandByte[1];
+    i2c_status_t    status;
+
+    if (deviceRegister > 0x05) {
+        return kWarpStatusBadDeviceCommand;
+    }
+
+    i2c_device_t slave =
+    {
+        .address = deviceINA219State.i2cAddress,
+        .baudRate_kbps = gWarpI2cBaudRateKbps
+    };
+
+    warpScaleSupplyVoltage(deviceINA219State.operatingVoltageMillivolts);
+    commandByte[0] = deviceRegister;
+    warpEnableI2Cpins();
+
+    status = I2C_DRV_MasterSendDataBlocking(
+                            0 /* I2C instance */,
+                            &slave,
+                            commandByte,
+                            1,
+                            NULL,
+                            0,
+                            gWarpI2cTimeoutMilliseconds);
+
+    if (status != kStatus_I2C_Success)
+    {
+        return kWarpStatusDeviceCommunicationFailed;
+    }
+
+    return kWarpStatusOK;
+}
+
+WarpStatus
+devINA219read(void)
+{
+    i2c_status_t    status;
+
+    USED(2);
+
+    i2c_device_t slave =
+    {
+        .address = deviceINA219State.i2cAddress,
+        .baudRate_kbps = gWarpI2cBaudRateKbps
+    };
+
+    warpScaleSupplyVoltage(deviceINA219State.operatingVoltageMillivolts);
+    warpEnableI2Cpins();
+
+    status = I2C_DRV_MasterReceiveDataBlocking(
+                            0 /* I2C instance */,
+                            &slave,
+                            NULL,
+                            0,
+                            (uint8_t *)deviceINA219State.i2cBuffer,
+                            2,
+                            gWarpI2cTimeoutMilliseconds);
+
+    if (status != kStatus_I2C_Success)
+    {
+        return kWarpStatusDeviceCommunicationFailed;
+    }
+
+    return kWarpStatusOK;
+}
+
+#if (INA219_DEBUG)
+    extern void warpPrint(const char *fmt, ...);
+#endif
+
+int
+devINA219getCurrent(void)
+{
+    uint16_t current_raw;
+    WarpStatus status;
+
+    /* set register pointer to current */
+    status = devINA219writeRegisterPointer(kINA219RegisterCurrent);
+    if (status != kWarpStatusOK) return 0; /* error condition  */
+
+    /* read from device */
+    status = devINA219read();
+    if (status != kWarpStatusOK) return 0; /* error condition  */
+
+    current_raw = (int16_t) (
+        deviceINA219State.i2cBuffer[1] |
+        deviceINA219State.i2cBuffer[0] << 8
+    );
+
+    return (int) (current_raw * INA219_CURRENT_LSB);
+}
+
+unsigned int
+devINA219getBusVoltage(void)
+{
+    ina219_reg_bus_voltage_t voltage_raw;
+    WarpStatus status;
+
+    /* set register pointer to current */
+    status = devINA219writeRegisterPointer(kINA219RegisterBusVoltage);
+    if (status != kWarpStatusOK) return 0; /* error condition  */
+
+    /* read from device */
+    status = devINA219read();
+    if (status != kWarpStatusOK) return 0; /* error condition  */
+
+    voltage_raw.lsb = deviceINA219State.i2cBuffer[1];
+    voltage_raw.msb = deviceINA219State.i2cBuffer[0];
+
+    /* LSB = 4mV */
+    return (unsigned int) (voltage_raw.bd * 4);
+}
+
+int
+devINA219getShuntVoltage(void)
+{
+    int16_t voltage_raw;
+    WarpStatus status;
+
+    /* set register pointer to current */
+    status = devINA219writeRegisterPointer(kINA219RegisterShuntVoltage);
+    if (status != kWarpStatusOK) return 0; /* error condition  */
+
+    /* read from device */
+    status = devINA219read();
+    if (status != kWarpStatusOK) return 0; /* error condition  */
+
+    voltage_raw = (int16_t) (
+        deviceINA219State.i2cBuffer[1] |
+        deviceINA219State.i2cBuffer[0] << 8
+    );
+
+    /* LSB = 10uV */
+    return (int) (voltage_raw * 10);
+}
+
+
+unsigned int
+devINA219getPower(void)
+{
+    uint16_t power_raw;
+    WarpStatus status;
+
+    /* set register pointer to current */
+    status = devINA219writeRegisterPointer(kINA219RegisterPower);
+    if (status != kWarpStatusOK) return 0; /* error condition  */
+
+    /* read from device */
+    status = devINA219read();
+    if (status != kWarpStatusOK) return 0; /* error condition  */
+
+    power_raw = (uint16_t) (
+        deviceINA219State.i2cBuffer[1] |
+        deviceINA219State.i2cBuffer[0] << 8
+    );
+
+    /* LSB = 4mV */
+    return (unsigned int) (power_raw * INA219_POWER_LSB);
+}
---
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -0,0 +1,134 @@
+/*
+ *
+ */
+
+typedef enum { /* register map */
+     kINA219RegisterConfig        = 0x00,
+     kINA219RegisterShuntVoltage  = 0x01,
+     kINA219RegisterBusVoltage    = 0x02,
+     kINA219RegisterPower         = 0x03,
+     kINA219RegisterCurrent       = 0x04,
+     kINA219RegisterCalibration   = 0x05,
+ } INA219Register;
+
+ #define INA219_CAL         0xA000
+//32V 2A config
+ #define INA219_CURRENT_LSB 10
+#define INA219_POWER_LSB   (20 * INA219_CURRENT_LSB)
+
+ typedef enum {
+     ina219_pg_range_40mv  = 0b00,
+     ina219_pg_range_80mv  = 0b01,
+     ina219_pg_range_160mv = 0b10,
+     ina219_pg_range_320mv = 0b11,
+ } INA219Range;
+
+ typedef enum {
+     ina219_adc_mode_9bit  = 0b0000,
+     ina219_adc_mode_10bit = 0b0001,
+     ina219_adc_mode_11bit = 0b0010,
+     ina219_adc_mode_12bit = 0b0011,
+
+     ina219_adc_samples_1   = 0b1000,
+     ina219_adc_samples_2   = 0b1001,
+     ina219_adc_samples_4   = 0b1010,
+     ina219_adc_samples_8   = 0b1011,
+     ina219_adc_samples_16  = 0b1100,
+     ina219_adc_samples_32  = 0b1101,
+     ina219_adc_samples_64  = 0b1110,
+     ina219_adc_samples_128 = 0b1111,
+ } INA219ADCconfig;
+
+ typedef enum {
+     ina219_mode_power_down     = 0b000,
+     ina219_mode_shunt_trig     = 0b001,
+     ina219_mode_bus_trig       = 0b010,
+     ina219_mode_shunt_bus_trig = 0b011,
+     ina219_mode_adc_off        = 0b100,
+     ina219_mode_shunt_cont     = 0b101,
+     ina219_mode_bus_cont       = 0b110,
+     ina219_mode_shunt_bus_cont = 0b111,
+ } INA219Mode;
+
+ typedef enum {
+     ina219_bus_voltage_range_16v = 0,
+     ina219_bus_voltage_range_32v = 1,
+ } INA219BusVRange;
+
+ typedef struct __attribute__((packed)) {
+     union {
+         struct {
+             INA219Mode mode:      3;
+             INA219ADCconfig sadc: 4;
+             INA219ADCconfig badc: 4;
+             INA219Range pg:       2;
+             INA219BusVRange brng: 1; //bit 13 of BRNG as per datasheet
+             uint16_t :            1;
+             uint16_t rst:         1;
+         } __attribute__((packed));
+         uint16_t raw_val;
+     };
+ } ina219_config_t;
+
+typedef struct __attribute__((packed)) {
+    union {
+        struct {
+            uint16_t ovf:  1; /* math overflow flag                           */
+            uint16_t cnvr: 1; /* conversion ready flag                        */
+            uint16_t:      1; /* UNUSED                                       */
+            uint16_t bd:  13; /* bus voltage data                             */
+        } __attribute__((packed));
+        struct {
+            uint8_t lsb, msb;
+        } __attribute__((packed));
+    };
+} ina219_reg_bus_voltage_t;
+
+typedef struct {
+    /* because reading the registers _should_ be performed in a certain order:
+     * 0. (shunt voltage)
+     * 1. bus voltage   - wait for conv ready
+     * 2. current
+     * 3. power         - clears conv ready
+     *
+     * we want to return a set of coherent readings together.
+     * bus voltage also includes a flag for conv overflow,
+     * so return that as an error indicator.
+     */
+    bool error;
+    unsigned int busVoltage, power;
+    int current, shuntVoltage;
+} ina219_reading_set_t;
+
+ static const ina219_config_t config = {
+     .mode = ina219_mode_shunt_bus_cont,
+     .sadc = ina219_adc_samples_1,
+     .badc = ina219_adc_mode_12bit,
+       .pg = ina219_pg_range_40mv,
+     .brng = ina219_bus_voltage_range_16v,
+      .rst = 0
+ };
+
+ void
+initINA219(const uint8_t i2cAddress, uint16_t operatingVoltageMillivolts);
+
+ WarpStatus
+ devINA219writeRegister(INA219Register deviceRegister, uint16_t payload);
+
+ WarpStatus
+ devINA219writeRegisterPointer(INA219Register deviceRegister);
+
+ WarpStatus
+ devINA219read(void);
+
+int
+devINA219getCurrent(void);
+
+unsigned int
+devINA219getBusVoltage(void);
+
+int
+devINA219getShuntVoltage(void);
+
+unsigned int
+devINA219getPower(void);
